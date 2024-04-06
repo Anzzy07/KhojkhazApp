70d9e4bccb38ac4a0eb3894a8304ee014bc643ae
Anzzy07 <anzel.acharya307@gmail.com>
2024-04-06 16:01:39 +0545

    Solving Package Error

--- components/AddPropertySection.tsx
+++ components/AddPropertySection.tsx
@@ -1,28 +1,50 @@
 import { KeyboardAwareFlatList } from 'react-native-keyboard-aware-scroll-view';
-import { Text, Input, Divider, Button } from '@ui-kitten/components';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, Input } from '@ui-kitten/components';
+import { View, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
+import { useState } from 'react';
+import { PickerItem } from 'react-native-woodpicker';
 
 import { Screen } from './Screen';
 import { ModalHeader } from './ModalHeader';
 import { Row } from './Row';
 import { UnitButton } from './UnitButton';
+import { SearchLocation } from 'types/locationIQ';
+import { SearchAddress } from './SearchAddress';
+import { getStateAbbreviation } from 'utils/getStateAbbreviation';
+import { Select } from './Select';
 
 export const AddPropertySection = () => {
+  const [searchingLocation, setSearchingLocation] = useState(false);
+  const [suggestions, setSuggestions] = useState<SearchLocation[]>([]);
+
+  const handleGoBack = () => {
+    setSearchingLocation(false);
+  };
+
   return (
     <KeyboardAwareFlatList
       data={[1]}
       renderItem={() => (
         <Screen>
-          <ModalHeader text="Khojkhaz" xShown />
+          {!searchingLocation && <ModalHeader text="Khojkhaz" xShown />}
           <View style={styles.container}>
-            <Text category={'h5'} style={styles.header}>
-              Add a Property
-            </Text>
+            {!searchingLocation && (
+              <Text category={'h5'} style={styles.header}>
+                Add a Property
+              </Text>
+            )}
 
             <Formik
               initialValues={{
                 unitType: 'single',
+                street: '',
+                city: '',
+                state: '',
+                zip: '',
+                lat: '',
+                lng: '',
+                propertyType: propertyTypes[0],
               }}
               onSubmit={(values) => console.log(values)}>
               {({
@@ -34,6 +56,49 @@ export const AddPropertySection = () => {
                 setFieldValue,
                 handleChange,
               }) => {
+                const onLocationFocus = () => {
+                  setSearchingLocation(true);
+                  setFieldTouched('street');
+                };
+
+                const handleSuggestionPress = (item: any) => {
+                  const location = item as SearchLocation;
+
+                  let street = location.address?.road;
+                  if (location.address?.house_number)
+                    street = `${location.address.house_number} ${street}`;
+
+                  // International use case (current Property Data Struct only really applies to USA)
+                  let state = location.address.state;
+                  if (!state) state = location.address.country;
+
+                  setFieldValue('street', street);
+                  setFieldValue('city', location.address.city);
+                  setFieldValue('state', state);
+                  setFieldValue('zip', location.address.postcode);
+                  setFieldValue('lat', location.lat);
+                  setFieldValue('lng', location.lon);
+
+                  handleGoBack();
+                };
+
+                const currentLocation = values.street
+                  ? values.state
+                    ? `${values.street}, ${values.city}, ${getStateAbbreviation(values.state)}`
+                    : `${values.street}, ${values.city}`
+                  : '';
+
+                if (searchingLocation)
+                  return (
+                    <SearchAddress
+                      type="search"
+                      suggestions={suggestions}
+                      setSuggestions={(item) => setSuggestions(item as SearchLocation[])}
+                      handleGoBack={handleGoBack}
+                      handleSuggestionPress={handleSuggestionPress}
+                      defaultLocation={currentLocation}
+                    />
+                  );
                 return (
                   <View>
                     <Row style={styles.row}>
@@ -50,6 +115,31 @@ export const AddPropertySection = () => {
                         onPress={() => setFieldValue('unitType', 'multiple')}
                       />
                     </Row>
+
+                    <Input
+                      placeholder="Property Address"
+                      label="Address"
+                      value={currentLocation}
+                      onFocus={onLocationFocus}
+                      style={styles.input}
+                      caption={
+                        !values.street && touched.street && errors.street
+                          ? errors.street
+                          : undefined
+                      }
+                      status={
+                        !values.street && touched.street && errors.street ? 'danger' : 'basic'
+                      }
+                    />
+
+                    <Select
+                      label="Property Type"
+                      item={values.propertyType}
+                      items={propertyTypes}
+                      onItemChange={(item) => setFieldValue('propertyType', item)}
+                      isNullable={false}
+                      style={styles.input}
+                    />
                   </View>
                 );
               }}
@@ -68,4 +158,18 @@ const styles = StyleSheet.create({
   row: {
     justifyContent: 'space-evenly',
   },
+  input: {
+    marginTop: 15,
+  },
 });
+
+const propertyTypes: PickerItem[] = [
+  { label: 'Apartment', value: 'Apartment' },
+  { label: 'Single Family House', value: 'Single Family House' },
+  { label: 'Land', value: 'Land' },
+  { label: 'Townhouse', value: 'Townhouse' },
+  {
+    label: 'Mobile Home / Manufactured Home',
+    value: 'Mobile Home / Manufactured Home',
+  },
+];
